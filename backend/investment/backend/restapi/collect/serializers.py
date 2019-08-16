c86f283f8b05ec2d0900a96e356f919e551b6cc5
jingluomaf <jingluomaf@gmail.com>
2019-08-16 19:56:30 +1200

    Moved graphql api to a new repo

--- backend/investment/backend/restapi/collect/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from collect.models import HistoricalData
-
-
-class HistoricalDataSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = HistoricalData
-        fields = '__all__'
