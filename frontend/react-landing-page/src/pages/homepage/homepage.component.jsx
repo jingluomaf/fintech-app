6c3d11675c41e9c12daeb85b0a6cc8c16c9f2e78
jingluomaf <jingluomaf@gmail.com>
2019-08-21 17:08:54 +1200

    Delete links on page

--- frontend/react-landing-page/src/pages/homepage/homepage.component.jsx
+++ frontend/react-landing-page/src/pages/homepage/homepage.component.jsx
@@ -7,11 +7,6 @@ import { HomePageContainer } from "./homepage.styles";
 const HomePage = () => (
   <HomePageContainer>
     <Directory />
-    <a href="http://localhost:8888">jupyter</a>
-    <a href="http://localhost:8000">django</a>
-    <a href="http://localhost:9000">minio</a>
-    <a href="http://localhost:9090">portainer</a>
-    <a href="http://localhost:8080">airflow</a>
   </HomePageContainer>
 );
 
