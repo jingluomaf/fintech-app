329cb3f46a232373058af8ba164f10bd851359be
jingluomaf <jingluomaf@gmail.com>
2019-08-27 11:43:46 +1200

    Fixed port conflict between portainer and minio

--- frontend/react-landing-page/src/components/header/header.component.jsx
+++ frontend/react-landing-page/src/components/header/header.component.jsx
@@ -53,7 +53,7 @@ export const Header = ({ currentUser, hidden, signOutStart }) => (
       </OptionWeb>
       <OptionWeb>
         <a
-          href="http://localhost:8080"
+          href="http://localhost:9090"
           target="_blank"
           rel="noopener noreferrer"
         >
